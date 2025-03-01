72f402575fc6bf308af41d945aff0bda4d938372
Deo silaen <deokeldisilaen@gmail.com>
2025-03-01 13:21:41 +0700

    addDetectMouse

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlaceable;
+    void OnMouseDown()
     {
-        
-    }
+        if (isPlaceable)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log(transform.name);
+        }
     }
 }
