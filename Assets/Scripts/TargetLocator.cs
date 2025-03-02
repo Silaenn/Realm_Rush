ad4a6f21b2bca2fbc66716014917d47d3a2c0883
Deo silaen <deokeldisilaen@gmail.com>
2025-03-02 12:25:04 +0700

    targetClosest

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,23 +6,59 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
+    [SerializeField]
+    ParticleSystem projectileParticles;
+    [SerializeField] float range = 15f;
     Transform target;
 
-    void Start()
-    {
-        target = FindObjectOfType<EnemyMover>().transform;
-    }
     void Update()
     {
+        FindClosetTarget();
         AimWeapon();
     }
 
-    private void AimWeapon()
+    void FindClosetTarget()
+    {
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Enemy enemy in enemies)
+        {
+            float targetDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (targetDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = targetDistance;
+            }
+        }
+
+        target = closestTarget;
+    }
+
+    void AimWeapon()
     {
         if (target != null)
         {
+            float targetDistance = Vector3.Distance(transform.position, target.position);
+
+            if (targetDistance < range)
+            {
+                Attack(true);
+            }
+            else
+            {
+                Attack(false);
+            }
 
             transform.LookAt(target.position);
         }
+    }
+
+    void Attack(bool isActive)
+    {
+        var emmisionModule = projectileParticles.emission;
+        emmisionModule.enabled = isActive;
     }
 }
